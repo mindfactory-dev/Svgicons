7bcd51ada56a170b80346f53ac1e51437a4d44c4
Henrik Persson <henrik.persson@mindfactory.se>
2024-05-01 18:41:01 +0200

    First commit

---
+++ src/View/Helper/IconHelper.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Mindfactory\Svgicons\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Icon helper
+ */
+class IconHelper extends Helper
+{
+    /**
+     * Default configuration.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [];
+}
---
+++ tests/TestCase/View/Helper/IconHelperTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Mindfactory\Svgicons\Test\TestCase\View\Helper;
+
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use Mindfactory\Svgicons\View\Helper\IconHelper;
+
+/**
+ * Mindfactory\Svgicons\View\Helper\IconHelper Test Case
+ */
+class IconHelperTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \Mindfactory\Svgicons\View\Helper\IconHelper
+     */
+    protected $Icon;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $view = new View();
+        $this->Icon = new IconHelper($view);
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        unset($this->Icon);
+
+        parent::tearDown();
+    }
+}
