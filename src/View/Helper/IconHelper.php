37ae18eb324936ece5bf2c1c3a94a45d366e5a90
Henrik Persson <henrik.persson@mindfactory.se>
2024-05-03 10:51:42 +0200

    Basic usage

--- src/View/Helper/IconHelper.php
+++ src/View/Helper/IconHelper.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Mindfactory\Svgicons\View\Helper;
@@ -16,5 +17,47 @@ class IconHelper extends Helper
      *
      * @var array<string, mixed>
      */
-    protected array $_defaultConfig = [];
+    protected array $_defaultConfig = [
+        'delimiter' => '.',
+        'iconSets' => [
+            'default' => 'heroicons/24/outline',
+        ],
+        'pathToNodeModulesFolder' => ROOT,
+    ];
+
+
+    public function get(string $icon): string
+    {
+
+        if (str_contains($icon, $this->getConfig('delimiter'))) {
+            $iconSet = substr($icon, 0, strrpos($icon, $this->getConfig('delimiter')));
+            $iconName = substr(strrchr($icon, $this->getConfig('delimiter')), 1);
+        } else {
+            $iconName = $icon;
+            $iconSet = 'default';
+        }
+        // debug($iconName);
+        // debug($iconSet);
+
+        $iconSets = $this->getConfig('iconSets');
+
+        // debug($iconSets);
+        // debug($iconSets[$iconSet]);
+
+        $selectedIconSet = array_key_exists($iconSet, $iconSets) ? trim($iconSets[$iconSet], '/') : '';
+
+        // debug($selectedIconSet);
+
+        $iconPath = rtrim($this->getConfig('pathToNodeModulesFolder'), '/') . DS . 'node_modules' . DS . $selectedIconSet . DS . $iconName . '.svg';
+
+        // debug($iconPath);
+
+        // debug(file_exists($iconPath));
+
+        if (!file_exists($iconPath)) {
+            return '';
+        }
+
+        return file_get_contents($iconPath);
+    }
 }
--- tests/TestCase/View/Helper/IconHelperTest.php
+++ tests/TestCase/View/Helper/IconHelperTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Mindfactory\Svgicons\Test\TestCase\View\Helper;
@@ -41,5 +42,119 @@ class IconHelperTest extends TestCase
         unset($this->Icon);
 
         parent::tearDown();
+    }
+
+    /** @test */
+    public function returns_icon_content(): void
+    {
+        $this->Icon->setConfig('iconSets', ['provider-a' => 'provider-a']);
+
+        $result = $this->Icon->get('provider-a.a-a');
+        $expected = 'svg-a-a';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function default_icon_set_when_no_one_provided(): void
+    {
+
+        $result = $this->Icon->get('heroicon');
+        $expected = 'svg-heroicon';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function default_icon_set_when_one_is_provided(): void
+    {
+        $this->Icon->setConfig('iconSets', ['default' => 'provider-a/']);
+
+        $result = $this->Icon->get('a-a');
+        $expected = 'svg-a-a';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function wrong_icon_name(): void
+    {
+        $result = $this->Icon->get('wrong-icon-name');
+        $expected = '';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function wrong_provider_name(): void
+    {
+        $result = $this->Icon->get('wrong-provider-name.icon');
+        $expected = '';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function with_other_delimiter(): void
+    {
+        $this->Icon->setConfig([
+            'iconSets' => [
+                'provider-a' => 'provider-a'
+            ],
+            'delimiter' => '/'
+        ]);
+
+        $result = $this->Icon->get('provider-a/a-a');
+        $expected = 'svg-a-a';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function node_modules_in_difrent_path(): void
+    {
+        $this->Icon->setConfig([
+            'iconSets' => [
+                'provider-b' => 'provider-b'
+            ],
+            'pathToNodeModulesFolder' => ROOT . 'path/to/node_modules/folder/',
+        ]);
+
+        $result = $this->Icon->get('provider-b.b-a');
+        $expected = 'svg-b-a';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function setting_with_slashes(): void
+    {
+        $this->Icon->setConfig([
+            'iconSets' => [
+                'provider-b' => '/provider-b/'
+            ],
+            'pathToNodeModulesFolder' => ROOT . 'path/to/node_modules/folder/',
+        ]);
+
+        $result = $this->Icon->get('provider-b.b-a');
+        $expected = 'svg-b-a';
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /** @test */
+    public function setting_without_slashes(): void
+    {
+        $this->Icon->setConfig([
+            'iconSets' => [
+                'provider-b' => 'provider-b'
+            ],
+            'pathToNodeModulesFolder' => ROOT . 'path/to/node_modules/folder',
+        ]);
+
+        $result = $this->Icon->get('provider-b.b-a');
+        $expected = 'svg-b-a';
+
+        $this->assertEquals($expected, $result);
     }
 }
